bd471ea3753dfa2ef962a9a49b63e7eef8135056
LynnMeza <lynn770450@gmail.com>
2024-01-12 08:46:18 -0500

    feat: Creation of data types and variables

---
+++ JavaScript-Junior/Study/Tipo_datos.js
@@ -0,0 +1,17 @@
+string = "Cade de texto o caracteres unidos"//se puede escribir con '' o  ""
+number =79
+booleano = true // puede tomar true o false
+
+/*Hosting 
+habla de como se habla las cosas
+*/
+
+let code= null //variable vacio y lo declaramos vacio
+
+let nuemro1=5
+let numero = "Jose"
+//NaN (Not a Number)
+
+let nombre= prompt("Dime tu nombre")//Pregunta despues del,  una funcion quew devuelve lo que escribimos
+alert("Hola "+ nombre)//Concatenacion
+console.log(nombre)//
---
+++ JavaScript-Junior/Study/variables.js
@@ -0,0 +1,17 @@
+var palabra ="Hola de codigo de JavaScript"
+
+//DECLARACIONES DE VARIABLES
+//var = alcance global, let = limita el ancalce a lo que ejecutamos ,const = constante 
+
+const nombre="Lynn"
+//nombre="Javier" si lo depuramos (saldra un error)
+
+let numero;//declaramos un variable
+let nuemro1, numero2, numero3//declaramos multiples variables
+numero= 30;//inicializamos la variable, si no inicializamos un valor a la declaracion nos dara  Undifane(Esta la declaracion pero no tine valor) por consola
+
+console.log(numero)
+
+
+
+
